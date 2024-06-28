229d6eaba10679bdbbb2e90c89a63051f03684b6
manju-bashini-m <163808871+manju-bashini-m@users.noreply.github.com>
2024-06-28 08:29:28 +0530

    Update app.ts

--- to-do-list/app.ts
+++ to-do-list/app.ts
@@ -1,15 +1,15 @@
-type Task = {
+type Task ={
     id: number;
     content: string;
 };
 
-const tasks:Task[] = [];
+const tasks:Task[] =[];
 let nextId = 1;
 
-function addTask():void {
+function addTask():void{
     const inputElement:HTMLInputElement = document.getElementById('todo-input') as HTMLInputElement;
     const newTaskContent:string = inputElement.value.trim();
-    if (newTaskContent) {
+    if (newTaskContent){
         const newTask:Task ={ id: nextId++, content: newTaskContent };
         tasks.push(newTask);
         updateTaskList();
@@ -18,21 +18,21 @@ function addTask():void {
     }
 }
 
-function updateTaskList():void {
+function updateTaskList():void{
     const listElement:HTMLElement =document.getElementById('todo-list');
     listElement.innerHTML=''; // Clear existing tasks in the list
 
-    for (const task of tasks) {
+    for (const task of tasks){
         const listItem =document.createElement('li');
         listItem.textContent =task.content;
-        listItem.onclick =() => { removeTask(task.id); };
+        listItem.onclick =() =>{ removeTask(task.id); };
         listElement.appendChild(listItem);
     }
 }
 
-function removeTask(taskId:number):void {
+function removeTask(taskId:number):void{
     const taskIndex = tasks.findIndex(task => task.id === taskId);
-    if (taskIndex>-1) {
+    if (taskIndex>-1){
         tasks.splice(taskIndex,1); // Remove the task
         updateTaskList(); // Update the list
     }
